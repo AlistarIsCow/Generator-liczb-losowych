b07a947b496802045f5c0cb11f74fd272f0b9645
AlistarIsCow <62110503+AlistarIsCow@users.noreply.github.com>
2022-06-22 14:27:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Generator_liczb_losowych
+{
+    class Program
+    {
+        public static int CheckIfInt(string valueAsString)
+        {
+            int valueAsInt;
+            while (!int.TryParse(valueAsString, out valueAsInt))
+            {
+                Console.WriteLine("This is not a number!");
+                valueAsString = Console.ReadLine();
+            }
+            return valueAsInt;
+        }
+        static void Main(string[] args)
+        {
+            int dices, rangeLow, rangeHigh;
+            var rand = new Random();
+
+            Console.WriteLine("Insert the number of dices: ");
+            string dicesAsString = Console.ReadLine();
+            dices = CheckIfInt(dicesAsString);
+
+            Console.WriteLine("Insert the lower range: ");
+            string rangeLowAsString = Console.ReadLine();
+            rangeLow = CheckIfInt(rangeLowAsString);
+
+            Console.WriteLine("Insert the higher range: ");
+            string rangeHighAsString = Console.ReadLine();
+            rangeHigh = CheckIfInt(rangeHighAsString);
+
+            for (int i = 0; i < dices; i++)
+            {
+                Console.WriteLine(rand.Next(rangeLow, rangeHigh + 1));
+            }
+        }
+    }
+}
